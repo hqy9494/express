ffd60cb03942a43572352085d252a4fd125fd61e
huangqingyuan <hqy9494@qq.com>
2019-08-30 18:23:47 +0800

    Fix:修复图片上传路径和请求路径

--- routes/upload.js
+++ routes/upload.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
  const fs = require('fs');
  const multer  = require('multer');
+const path = require('path');
 
 
 
-
-var storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'upload_tmp/');
     },
@@ -20,11 +20,16 @@ var storage = multer.diskStorage({
 
 
   router.post('/image', upload.any(), function(req, res, next){
-    res.end(`http://127.0.0.1:3001`)
-})
+    let src = path.join(__dirname, '../upload_tmp/' + req.files[0].originalname);
+    res.end(src)
+  })
 
-    router.get('/download',function(req, res, next){
-        res.end(`http://127.0.0.1:3001`)
+    router.get('/image/:id',function(req, res, next){
+        // console.log(req)
+        let file = path.join(__dirname, '../upload_tmp/' + req.params.id);
+        res.sendFile( file )
+        // res.end(file)
+        // res.download(file);
     })
  
  module.exports = router;
